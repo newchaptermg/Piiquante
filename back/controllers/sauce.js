8db764b9324804b07477ac679fa2b490c86db4cc
MG <mohankumar.g31@gmail.com>
2024-08-30 15:19:20 -0400

    completed milestone 5

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -62,3 +62,36 @@ exports.getAllSauces = (req, res, next) => {
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.likeSauce = (req, res, next) => {
+  const userId = req.auth.userId;
+  const like = req.body.like;
+
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      if (like === 1) {
+        if (!sauce.usersLiked.includes(userId)) {
+          sauce.usersLiked.push(userId);
+          sauce.likes += 1;
+        }
+      } else if (like === -1) {
+        if (!sauce.usersDisliked.includes(userId)) {
+          sauce.usersDisliked.push(userId);
+          sauce.dislikes += 1;
+        }
+      } else if (like === 0) {
+        if (sauce.usersLiked.includes(userId)) {
+          sauce.likes -= 1;
+          sauce.usersLiked = sauce.usersLiked.filter(id => id !== userId);
+        } else if (sauce.usersDisliked.includes(userId)) {
+          sauce.dislikes -= 1;
+          sauce.usersDisliked = sauce.usersDisliked.filter(id => id !== userId);
+        }
+      }
+
+      sauce.save()
+        .then(() => res.status(200).json({ message: 'Sauce updated!' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(404).json({ error }));
+};
--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -10,5 +10,7 @@ router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
+
 
 module.exports = router;
